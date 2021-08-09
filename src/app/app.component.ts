f8ff1d6cedab72fbfbc4438f57713ce67bfcdd26
Md. Ehtisham Farooqui <mefxe01@gmail.com>
2021-08-09 22:31:18 +0530

    added add todo functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-angular-app';
+
+  todoArray: string[] = [];
+  todoInput: any;
+  //todoForm: new FormGroup()
+
+
+
+  // /*delete item*/
+  deleteItem(todo: string) {
+    // for (let i = 0; i <= this.todoArray.length; i++) {
+    //   if (todo == this.todoArray[i]) {
+    //     this.todoArray.splice(i, 1)
+    //   }
+    // }
+
+    console.log("delete todo " + todo)
+  }
+
+  // submit Form
+  todoSubmit(value: string) {
+    if (value !== "") {
+      this.todoArray.push(value)
+      //this.todoForm.reset()
+    } else {
+      alert('Todo is required')
+    }
+  }
+
+
 }
