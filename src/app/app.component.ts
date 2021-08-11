de9b77de76700295f52466b7905c0aef90542eec
Md. Ehtisham Farooqui <mefxe01@gmail.com>
2021-08-11 11:00:48 +0530

    added the local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
 @Component({
@@ -6,23 +6,33 @@ import { FormGroup } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'todo-angular-app';
 
-  todoArray: string[] = [];
+  todoArray: string[] | any = [];
   todoInput: any;
 
+  ngOnInit() {
+    if (localStorage.getItem('todos')) {
+      this.todoArray = JSON.parse(localStorage.getItem('todos') || '{}');
+    }
+  }
 
 
   // /*delete item*/
   deleteItem(todo: string) {
-    this.todoArray = this.todoArray.filter(val => val !== todo)
+    let temp = JSON.parse(localStorage.getItem('todos') || '{}');
+    localStorage.clear()
+    this.todoArray = temp.filter((val: string) => val !== todo)
+    localStorage.setItem('todos', JSON.stringify(this.todoArray))
+
   }
 
   // submit Form
   todoSubmit(value: string) {
     if (value) {
-      this.todoArray.push(value)
+      this.todoArray.push(value);
+      localStorage.setItem('todos', JSON.stringify(this.todoArray))
     } else {
       alert('Todo is required')
     }
